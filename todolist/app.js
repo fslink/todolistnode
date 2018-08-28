ae2488698e228433792eb0e9a5839e1b8e791831
unknown <mowglysofiann@hotmail.com>
2018-08-28 16:57:19 +0200

    0 to 1

--- todolist/app.js
+++ todolist/app.js
@@ -1,7 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
--- todolist/routes/index.js
+++ todolist/routes/index.js
@@ -1,84 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-var compteur = 0;
-var list = require('../data/tasks.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var result = checkCookie(req, res);
-	console.log(result);
-	var listPerso = [];
-	list.forEach(function(element){
-		if(element){
-			if(element.id == req.cookies.id){
-				listPerso.push(element);
-			}
-		}
-	});
+	res.send("Bienvenue sur la todo");
+})
 
-	res.render('index', { title: 'Liste des tâches', listPerso});
-});
+.get('/todo', function(req, res, next){
+	console.log(req.session.todolist);
+})
 
-router.get('/addTask', function(req, res, next){
-	var result = checkCookie(req, res);
-	console.log(result);
-	if(req.query.task){
-		result = addTaskJson(req.cookies.id, req.query.task);
-		console.log(result);
-	}
-	res.redirect('/');
-});
-
-router.get('/deleteTask', function(req, res, next){
-	var result = checkCookie(req, res);
-	list.forEach(function(element, index){
-		if(element){
-			console.log(element);
-			console.log(req.query.idTask);
-			if(element.id == req.query.idTask){
-				delete list[index];
-			}
-		}
-	});
-	var data = JSON.stringify(list);
-	fs.writeFileSync('./data/tasks.json', data);
-	res.redirect('/');
-});
-
-// add a task in tasks.json with the user id
-function addTaskJson(id, newTask){
-	try{
-		var row = {"id": id, "task": newTask};
-		list.push(row);
-		var data = JSON.stringify(list);
-		fs.writeFileSync('./data/tasks.json', data);
-		return 'data added';
-
-	} catch(e){
-		return e;
-	}
-}
-
-function checkCookie(req, res){
-  // check if client sent cookie
-  var result = "";
-  var cookie = req.cookies.id;
-  if (cookie === undefined){
-    // no: set a new cookie
-  	var randomNumber=Math.random().toString();
-	randomNumber=randomNumber.substring(2,randomNumber.length);
-	res.cookie('id', randomNumber, { maxId: 900000, httpOnly: true });
-	result = 'cookie created successfully';
-
-  } 
-  else
-  {
-    // yes, cookie was already present
-    result = 'cookie already present';
-  }
-
-  return result;
-}
+.post('/todo/ajouter', function(req, res, next){
+})
 
 module.exports = router;
