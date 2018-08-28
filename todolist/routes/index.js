6d2fd90e8a523f6ca14ea7f64de8a9f389d7582f
Ilyes Chehaibou <ilyeschehaibou@mbp-de-ilyes.home>
2018-08-28 20:36:59 +0200

    ejs index fixed

--- todolist/routes/index.js
+++ todolist/routes/index.js
@@ -3,15 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	req.session.todolist.push({task: 'manger des fraises'});
 	res.send("Bienvenue sur la todo");
 })
 
 .get('/todo', function(req, res, next){
-	console.log(req.session.todolist);
+	res.render('index', {title: 'To do List', todolist: req.session.todolist});
 })
 
 .post('/todo/ajouter', function(req, res, next){
+	req.session.todolist.push();
+
 })
 
 module.exports = router;
